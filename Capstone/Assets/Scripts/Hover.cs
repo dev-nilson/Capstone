8d61aaad6d25fc6298d43ed3315d62c3ca2b1b7d
Maggie Heusel <mheusel1@harding.edu>
2022-01-22 16:31:46 -0600

    Implement hover effect

---
+++ Capstone/Assets/Scripts/Hover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    Color MouseOverColor = Color.green;
+    Color Original;
+
+    //get GameObject’s material and color
+    MeshRenderer Renderer;
+
+    void Start()
+    {
+        //get mesh renderer component
+        Renderer = GetComponent<MeshRenderer>();
+        //get original color of the GameObject
+        Original = Renderer.material.color;
+    }
+
+    void OnMouseOver()
+    {
+        Renderer.material.color = MouseOverColor;
+    }
+
+    void OnMouseExit()
+    {
+        //reset color
+        Renderer.material.color = Original;
+    }
+}
