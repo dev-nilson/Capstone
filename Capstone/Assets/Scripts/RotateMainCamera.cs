72f649d07a431c86863f6f9144b309900998907a
Maggie Heusel <mheusel1@harding.edu>
2022-01-25 20:54:46 -0600

    Add buttons to rotate board

--- Capstone/Assets/Scripts/RotateMainCamera.cs
+++ Capstone/Assets/Scripts/RotateMainCamera.cs
@@ -7,6 +7,8 @@ public class RotateMainCamera : MonoBehaviour
 {
 	public Button rotateRight;
 	public Button rotateLeft;
+	public Button rotateUp;
+	public Button rotateDown;
 
 	void Start()
 	{
@@ -15,6 +17,12 @@ public class RotateMainCamera : MonoBehaviour
 
 		Button left = rotateLeft.GetComponent<Button>();
 		left.onClick.AddListener(rotateLeftClick);
+
+		Button up = rotateUp.GetComponent<Button>();
+		up.onClick.AddListener(rotateUpClick);
+
+		Button down = rotateDown.GetComponent<Button>();
+		down.onClick.AddListener(rotateDownClick);
 	}
 
 	void rotateRightClick()
@@ -25,5 +33,14 @@ public class RotateMainCamera : MonoBehaviour
 	void rotateLeftClick()
 	{
 		Debug.Log("You have clicked the left button!");
+	}
+	void rotateUpClick()
+	{
+		Debug.Log("You have clicked the up button!");
+	}
+
+	void rotateDownClick()
+	{
+		Debug.Log("You have clicked the down button!");
 	}
 }
