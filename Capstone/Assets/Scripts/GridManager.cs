5630bf9aa204e938269072bb5aabb3607414e801
Maggie Heusel <mheusel1@harding.edu>
2022-01-26 12:59:32 -0600

    Create Game 2D Grid

---
+++ Capstone/Assets/Scripts/GridManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int[,] Grid;
+    int Col = 5, Row = 5;
+    int Horizontal = 5, Vertical = 5;
+    public GameObject prefab;
+
+    void Start()
+    {
+        Grid = new int[Col, Row];
+        CreateEmptyBoard();
+    }
+
+    private void CreateEmptyBoard()
+    {
+        for (var i = 0; i < Col; i++)
+        {
+            for (var j = 0; j < Row; j++)
+            {
+                Grid[i, j] = Random.Range(0, 10);
+                GameObject tile = new GameObject("X: " + i + "Y: " + j);
+                Instantiate(prefab, new Vector3(i, j, 0), Quaternion.identity);
+                tile.transform.position = new Vector3(i - (Horizontal + 1f), j - (Vertical - 1f));
+            }
+        }
+    }
+}
