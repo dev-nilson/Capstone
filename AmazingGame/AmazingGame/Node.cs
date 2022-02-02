41c27c96e7ed251b97fb45509638485c7bee9df9
Denilson Lemus <dlemus@harding.edu>
2022-02-02 14:51:44 -0600

    Create Node class
    Nodes will represent possible moves and builds

--- AmazingGame/AmazingGame/Node.cs
+++ AmazingGame/AmazingGame/Node.cs
@@ -6,5 +6,12 @@ namespace AmazingGame
 {
     class Node
     {
+        public int score = 0;
+        public List<Node> children;
+
+        public Node(int score)
+        {
+            this.score = score;
+        }
     }
 }
