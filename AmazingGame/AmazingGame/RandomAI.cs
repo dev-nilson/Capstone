d4440e9df08eec5cc116e68b1f5109afed8a364c
Denilson Lemus <dlemus@harding.edu>
2022-02-14 19:36:03 -0600

    Create function to move randomly

---
+++ AmazingGame/AmazingGame/RandomAI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmazingGame
+{
+    class RandomAI
+    {
+        public GameBoard.Coordinates MoveRandomly(GameBoard.Coordinates[] playerPawns, GameBoard gameBoard)
+        {
+            GameBoard.Coordinates coordinates;
+            Random random = new Random();
+            int pawnIndex = -1;
+
+            //  both workers have available moves
+            if (gameBoard.AvailableMoves(playerPawns[0]).Count > 0 && gameBoard.AvailableMoves(playerPawns[1]).Count > 0)
+                pawnIndex = random.Next(0, 2);
+            //  only one worker has available moves
+            else if (gameBoard.AvailableMoves(playerPawns[0]).Count > 0)
+                pawnIndex = 0;
+            //  only one worker has available moves
+            else if (gameBoard.AvailableMoves(playerPawns[1]).Count > 0)
+                pawnIndex = 1;
+
+            List<GameBoard.Coordinates> availableMoves = gameBoard.AvailableMoves(playerPawns[pawnIndex]);
+            int coordinatesIndex = random.Next(0, availableMoves.Count);
+            coordinates = availableMoves[coordinatesIndex];
+
+            return coordinates;
+        }
+    }
+}
