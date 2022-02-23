ff4445f0b43018c6307058e92e2cfbb5a8c715af
lauragraceashburn <lauragraceashburn@gmail.com>
2022-02-22 21:17:00 -0600

    Work on tutorial, players, and quick game

---
+++ Capstone/Assets/Scripts/QuickGameScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class QuickGameScreen : MonoBehaviour
+{
+    public Button startGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button startGameBtn = startGame.GetComponent<Button>();
+        startGameBtn.onClick.AddListener(startGameClicked);
+    }
+
+    void startGameClicked()
+    {
+        Debug.Log("here");
+        SceneManager.LoadScene("GameBoard");
+    }
+}
