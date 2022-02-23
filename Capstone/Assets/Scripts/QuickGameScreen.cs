cefd7fc8849cd0e59cc9a4bf9965f07ba12ac332
lauragraceashburn <lauragraceashburn@gmail.com>
2022-02-22 21:58:17 -0600

    Work on displaying player during quick game

--- Capstone/Assets/Scripts/QuickGameScreen.cs
+++ Capstone/Assets/Scripts/QuickGameScreen.cs
@@ -7,17 +7,31 @@ using UnityEngine.UI;
 public class QuickGameScreen : MonoBehaviour
 {
     public Button startGame;
+    public Button pharoah;
+    public GameObject pharoah_H;
+    public GameObject pharoah_figure;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Button startGameBtn = startGame.GetComponent<Button>();
         startGameBtn.onClick.AddListener(startGameClicked);
+
+        Button pharoahBtn = pharoah.GetComponent<Button>();
+        pharoahBtn.onClick.AddListener(pharoahClicked);
     }
 
     void startGameClicked()
     {
         Debug.Log("here");
         SceneManager.LoadScene("GameBoard");
+    }
+
+    void pharoahClicked()
+    {
+        pharoah_H.SetActive(true);
+        pharoah_figure.SetActive(true);
+
     }
 }
