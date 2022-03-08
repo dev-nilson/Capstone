49885abd4d82bfa93f6fa6be1a284e719ca43f34
Maggie Heusel <mheusel1@harding.edu>
2022-03-07 21:15:37 -0600

    Fix board rotation so that it is accurate

--- Capstone/Assets/Scripts/RotateMainCamera.cs
+++ Capstone/Assets/Scripts/RotateMainCamera.cs
@@ -24,85 +24,84 @@ public class RotateMainCamera : MonoBehaviour
 		boardController = GameObject.Find("GridManager");
 		Board = boardController.GetComponent<GridManager>();
 	}
-
-	void rotateRightClick()
-	{
-		if(x == 0)
+    void rotateLeftClick()
+    {
+        if (x == 0)
         {
-			x = -5;
-			z = 25;
-			Board.transform.position = new Vector3(x, 0f, z);
-			Board.transform.rotation = Quaternion.Euler(90, 90, 0);
-		}
-		else if (x == -5)
-		{
-			x = 20;
-			z = 30;
-			Board.transform.position = new Vector3(x, 0f, z);
-			Board.transform.rotation = Quaternion.Euler(90, 0, 180);
-		}
-		else if (x == 20)
-		{
-			x = 25;
-			z = 5;
-			Board.transform.position = new Vector3(x, 0f, z);
-			Board.transform.rotation = Quaternion.Euler(90, 0, -270);
-		}
-		else if (x == 25)
-		{
-			x = 0;
-			z = 0;
-			Board.transform.position = new Vector3(x, 0f, z);
-			Board.transform.rotation = Quaternion.Euler(90, 0, 0);
-		}
+            x = -5;
+            z = 25;
+            Board.transform.position = new Vector3(x, 0f, z);
+            Board.transform.rotation = Quaternion.Euler(90, 90, 0);
+        }
+        else if (x == -5)
+        {
+            x = 20;
+            z = 30;
+            Board.transform.position = new Vector3(x, 0f, z);
+            Board.transform.rotation = Quaternion.Euler(90, 0, 180);
+        }
+        else if (x == 20)
+        {
+            x = 25;
+            z = 5;
+            Board.transform.position = new Vector3(x, 0f, z);
+            Board.transform.rotation = Quaternion.Euler(90, 0, -270);
+        }
+        else if (x == 25)
+        {
+            x = 0;
+            z = 0;
+            Board.transform.position = new Vector3(x, 0f, z);
+            Board.transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
 
-		Debug.Log("You have clicked the right button!");
+        Debug.Log("You have clicked the right button!");
 
-		GameObject[] aliens = GameObject.FindGameObjectsWithTag("Alien");
-		foreach (GameObject go in aliens)
-		{
-			go.transform.rotation = Quaternion.Euler(0, 0, 0);
-		}
-	}
+        GameObject[] aliens = GameObject.FindGameObjectsWithTag("Alien");
+        foreach (GameObject go in aliens)
+        {
+            go.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
 
-	void rotateLeftClick()
-	{
-		if (x == 0)
-		{
-			x = 25;
-			z = 5;
-			Board.transform.position = new Vector3(x, 0f, z);
-			Board.transform.rotation = Quaternion.Euler(90, 0, 90);
-		}
-		else if (x == 25)
-		{
-			Debug.Log("HERE");
+    void rotateRightClick()
+    {
+        if (x == 0)
+        {
+            x = 25;
+            z = 5;
+            Board.transform.position = new Vector3(x, 0f, z);
+            Board.transform.rotation = Quaternion.Euler(90, 0, 90);
+        }
+        else if (x == 25)
+        {
+            Debug.Log("HERE");
 
-			x = 20;
-			z = 30;
-			Board.transform.position = new Vector3(x, 0f, z);
-			Board.transform.rotation = Quaternion.Euler(90, 0, 180);
-		}
-		else if (x == 20)
-		{
-			x = -5;
-			z = 25;
-			Board.transform.position = new Vector3(x, 0f, z);
-			Board.transform.rotation = Quaternion.Euler(90, 0, 270);
-		}
-		else if (x == -5)
-		{
-			x = 0;
-			z = 0;
-			Board.transform.position = new Vector3(x, 0f, z);
-			Board.transform.rotation = Quaternion.Euler(90, 0, 0);
-		}
-		Debug.Log("You have clicked the left button!");
+            x = 20;
+            z = 30;
+            Board.transform.position = new Vector3(x, 0f, z);
+            Board.transform.rotation = Quaternion.Euler(90, 0, 180);
+        }
+        else if (x == 20)
+        {
+            x = -5;
+            z = 25;
+            Board.transform.position = new Vector3(x, 0f, z);
+            Board.transform.rotation = Quaternion.Euler(90, 0, 270);
+        }
+        else if (x == -5)
+        {
+            x = 0;
+            z = 0;
+            Board.transform.position = new Vector3(x, 0f, z);
+            Board.transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
+        Debug.Log("You have clicked the left button!");
 
-		GameObject[] aliens = GameObject.FindGameObjectsWithTag("Alien");
-		foreach (GameObject go in aliens)
-		{
-			go.transform.rotation = Quaternion.Euler(0, 0, 0);
-		}
-	}
+        GameObject[] aliens = GameObject.FindGameObjectsWithTag("Alien");
+        foreach (GameObject go in aliens)
+        {
+            go.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
 }
