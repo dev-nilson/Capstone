fd685d60912b19d5a8e5c2e7abbf239f26b2a895
Maggie Heusel <mheusel1@harding.edu>
2022-03-10 18:04:31 -0600

    Add setting scroll to story mode

--- Capstone/Assets/Scripts/StoryModeScreen.cs
+++ Capstone/Assets/Scripts/StoryModeScreen.cs
@@ -1,26 +1,36 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine;
+using System.Collections;
+using static GameUtilities;
 
 public class StoryModeScreen : MonoBehaviour
 {
     public Button back;
     public Button settings;
+    public Button tutorial;
     public Button exitSettings;
-    public GameObject settingsPopUp;
+
+    public GameObject scroll1;
+    public GameObject scroll2;
+    public GameObject scroll3;
+    public GameObject scroll4;
+    public GameObject scroll5;
+    public GameObject scroll6;
 
     void Start()
     {
-        settingsPopUp.SetActive(false);
-
         Button backBtn = back.GetComponent<Button>();
         backBtn.onClick.AddListener(backClicked);
 
         Button settingsBtn = settings.GetComponent<Button>();
         settingsBtn.onClick.AddListener(settingsClicked);
 
+        Button tutorialBtn = tutorial.GetComponent<Button>();
+        tutorialBtn.onClick.AddListener(tutorialClicked);
+
         Button exitSettingsBtn = exitSettings.GetComponent<Button>();
-        exitSettingsBtn.onClick.AddListener(exitSettingsClicked);
+        exitSettingsBtn.onClick.AddListener(delayDisplay);
     }
 
     void backClicked()
@@ -30,15 +40,68 @@ public class StoryModeScreen : MonoBehaviour
         SceneManager.LoadScene("Menu");
     }
 
-    void settingsClicked()
+    void tutorialClicked()
     {
-        Debug.Log("settings game");
-        settingsPopUp.SetActive(true); // false to hide, true to show
+        Debug.Log("tutorial");
     }
 
-    void exitSettingsClicked()
+    void settingsClicked()
     {
-        Debug.Log("exit");
-        settingsPopUp.SetActive(false); // false to hide, true to show
+        scroll1.SetActive(true);
+
+        //switch phases to turn off build and place player to create a fake modal pop up box
+        StorePhases();
+        DisablePhases();
+    }
+
+    void delayDisplay()
+    {
+        StartCoroutine(exitSettingsClicked());
+    }
+
+    IEnumerator exitSettingsClicked()
+    {
+        //time that the scroll waits before it moves on to the next image
+        float delay = .001f;
+
+        GameObject[] scrollArray = new GameObject[7];
+
+        //Get the Rectransform of each height in order to get the height 
+        scrollArray[1] = scroll1;
+        scrollArray[2] = scroll2;
+        scrollArray[3] = scroll3;
+        scrollArray[4] = scroll4;
+        scrollArray[5] = scroll5;
+        scrollArray[6] = scroll6;
+
+        //Create a copy of the scrolls in order to reset the heights back to the original
+        GameObject[] scrollReset = scrollArray;
+
+        //BEAUTIFUL PIECE OF CODE --- Love Dad
+        for (int scrollNum = 1; scrollNum <= 5; scrollNum++)
+        {
+            scrollArray[scrollNum].SetActive(true);
+            float firstScrollRef = scrollArray[scrollNum].GetComponent<RectTransform>().rect.height;
+            float tempHeight = firstScrollRef;
+            float secondScrollRef = scrollArray[scrollNum + 1].GetComponent<RectTransform>().rect.height;
+            for (float i = firstScrollRef; i >= (firstScrollRef - ((firstScrollRef - secondScrollRef))); i -= 8)
+            {
+                scrollArray[scrollNum].GetComponent<RectTransform>().sizeDelta = new Vector2(scrollArray[scrollNum].GetComponent<RectTransform>().rect.width, i);
+                yield return new WaitForSecondsRealtime(delay);
+            }
+            scrollArray[scrollNum].SetActive(false);
+
+            //add delay before is turns off
+            if (scrollNum == 5)
+            {
+                scrollArray[6].SetActive(true);
+                yield return new WaitForSeconds(.1f);
+                scrollArray[6].SetActive(false);
+            }
+            //reset height value
+            scrollArray[scrollNum].GetComponent<RectTransform>().sizeDelta = new Vector2(scrollReset[scrollNum].GetComponent<RectTransform>().rect.width, tempHeight);
+        }
+
+        RestorePhases();
     }
 }
